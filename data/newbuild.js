a7169e600c60b78e9f1f6bdba3d55641daee5188
Tairraos <xiaole.tao@qq.com>
2023-08-17 18:04:45 +0800

    支持Excel更多sheet管理

--- data/newbuild.js
+++ data/newbuild.js
@@ -14,19 +14,26 @@ let workbook = XLSX.readFile(`${__dirname}/prompt.xlsx`, { type: "binary" }),
         命令: "command",
         负面: "eg"
     },
-    sheetNameList = Object.keys(subTypeMap),
+    sheetNameList = workbook.SheetNames,
     mapDict = {};
 
 sheetNameList.forEach((sheetName) => {
+    let subTypeName = sheetName.replace(/[-\.].*/, ""),
+        subType = subTypeMap[subTypeName];
+    if (!subType) {
+        console.log(`找不至 "${sheetName}" 对应的 subType，跳过。`)
+        return; //如果sheet不是以subType名字开头的话，就跳过
+    }
+    console.log(`${sheetName}" 对应 ${subType} 类型，处理开始。`)
     let ws = workbook.Sheets[sheetName],
-        lenth = XLSX.utils.decode_range(ws["!ref"]).e.r;                  //How much lines of this sheet
+        lenth = XLSX.utils.decode_range(ws["!ref"]).e.r;                    //How much lines of this sheet
     Array.from(Array(lenth), (_, i) => i + 2).forEach((id) => {
-        let prompt = { text: ws[`A${id}`].v };                            //text
-        ws[`B${id}`] && (prompt.lang_zh = ws[`B${id}`].v);                //lang_zh
-        prompt.subType = subTypeMap[sheetName];                           //subType
-        ws[`E${id}`] && (prompt.desc = ws[`E${id}`].v);                   //desc
-        ws[`D${id}`] && (prompt.sampleCmds = JSON.parse(ws[`D${id}`].v)); //sampleCmds
-        prompt.dir = `${sheetName}/${ws[`C${id}`].v}`;                    //dir
+        let prompt = { text: ws[`A${id}`].v };                              //text
+        ws[`B${id}`] && (prompt.lang_zh = ws[`B${id}`].v);                  //lang_zh
+        prompt.subType = subType;                                           //subType
+        ws[`E${id}`] && (prompt.desc = ws[`E${id}`].v);                     //desc
+        ws[`D${id}`] && (prompt.sampleCmds = JSON.parse(ws[`D${id}`].v));   //sampleCmds
+        prompt.dir = `${subTypeName}/${ws[`C${id}`].v}`;                    //dir
         mapDict[ws[`A${id}`].v] = prompt;
     });
 });
